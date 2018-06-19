2eae7d70ead211dc7b1c0d869ef402eee5a90f3d
Byron <byron@atebol.com>
2018-06-19 18:55:42 +0100

    fix to tabs

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     });
 
     $('.nav-link').on('click', function(){
-        localStorage.setItem('tabs', $(this).attr('id'));
+        localStorage.setItem('tabs', $(this).parent().index());
     });
 });
 
@@ -13,17 +13,17 @@ function checkNavTabs($block) {
     var $tabs = $block.find('.nav-link');
     var $panes = $block.find('.tab-pane');
 
-    if (localStorage.getItem('tabs') == "") {
+    if (localStorage.getItem('tabs') === null) {
         $tabs.first().addClass('show active');
         $panes.first().addClass('show active');
         localStorage.setItem('tabs', $tabs.first().attr('id'));
 
     } else {
-        var tabID = localStorage.getItem('tabs');
-        console.log(tabID);
-        var $tab = $('#'+tabID);
+        var index = localStorage.getItem('tabs');
+        var $tab = $tabs.eq(index);
         $tab.addClass('show active');
         $($tab.attr('href')).addClass('show active');
     }
+    localStorage.clear();
 }
 
