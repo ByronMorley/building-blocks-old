8fa886030c826770c07bab1dac63004fd3e57023
Byron <byron@atebol.com>
2018-07-18 16:40:46 +0100

    update to wordbank

---
+++ php/download.php
@@ -0,0 +1,63 @@
+<html><head><meta charset="UTF-8"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link href="../css/pdf.css" type="text/css" media="screen" rel="stylesheet"></head><body><div class="pdf-container"><h1></h1><h2></h2><div class="breadcrumbnavigation">
+		<span class="first"><a href="/Tourism/">Home</a></span>  » 
+	
+		<span class=""><a href="/Tourism/home/main-menu/">Main Menu</a></span>  » 
+	
+		<span class=""><a href="/Tourism/home/main-menu/be-better-at-the-business-of-tourism/">Unit 2</a></span>  » 
+	
+		<span class=""><a href="/Tourism/home/main-menu/be-better-at-the-business-of-tourism/ac-2-2/">AC 2.2</a></span>  » 
+	
+		<span class=""><a href="/Tourism/home/main-menu/be-better-at-the-business-of-tourism/ac-2-2/resources-3/">Resources</a></span>  » 
+	
+		<span class="last">Meeting expectations</span> 
+	</div><div class="block">
+				<div class="pane round content utility-bar">
+                    <div class="pane title-pane">
+	<div class="section left rel">
+		<h2 class="title abs bottom left">Meeting expectations</h2>
+	</div>
+	<div class="section right">
+		<div class="icon-rack-wrapper">
+			<ul class="icon-rack abs bottom right">
+				<li><a href="building-blocks/php/pdf_download.php" target="_blank" class="tabloid_download"><i class="icon-printer"></i></a></li>
+				<li><a href="building-blocks/php/pdf_download.php" target="_blank" class="tabloid_download"><i class="icon-download"></i></a></li>
+                <li><a href="javascript:void(0)"><i class="icon-presentation"></i></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+                    
+	<div class="breadcrumbnavigation">
+		<span class="first"><a href="/Tourism/">Home</a></span>  » 
+	
+		<span class=""><a href="/Tourism/home/main-menu/">Main Menu</a></span>  » 
+	
+		<span class=""><a href="/Tourism/home/main-menu/be-better-at-the-business-of-tourism/">Unit 2</a></span>  » 
+	
+		<span class=""><a href="/Tourism/home/main-menu/be-better-at-the-business-of-tourism/ac-2-2/">AC 2.2</a></span>  » 
+	
+		<span class=""><a href="/Tourism/home/main-menu/be-better-at-the-business-of-tourism/ac-2-2/resources-3/">Resources</a></span>  » 
+	
+		<span class="last">Meeting expectations</span> 
+	</div>
+
+				</div>
+				</div><div class="block text-block">
+    
+		<div class="pane round content typography">
+            
+				<h2>Meeting expectations</h2>
+            
+            <p>Very often tourists buy a tourism product without actually seeing it.&nbsp; This is the case with a holiday or a night’s accommodation.</p><p>Each of the tourists below booked to stay in a hostel in East London.&nbsp; As you will see, the reviews they left on Trip Advisor were very different.&nbsp; Some gave the accommodation 1 star while others gave it 5 stars. &nbsp;&nbsp;(They are real reviews!)</p>
+		</div>
+    
+</div><div class="block text-block">
+    
+		<div class="pane round content typography">
+            
+				<h2>Activity placeholder</h2>
+            
+            <p>Read the 6 reviews and then for each, use the writing frame to write a sentence or two explaining how the expectations of the tourist were met or not met by the hostel.</p>
+		</div>
+    
+</div></div></body></html>
---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.tab-block').each(function () {
+        checkNavTabs($(this));
+    });
+
+    $('.nav-link').on('click', function(){
+        localStorage.setItem('tabs', $(this).parent().index());
+    });
+});
+
+function checkNavTabs($block) {
+
+    var $tabs = $block.find('.nav-link');
+    var $panes = $block.find('.tab-pane');
+
+    if (localStorage.getItem('tabs') === null) {
+        console.log('null');
+        $tabs.first().addClass('show active');
+        $panes.first().addClass('show active');
+        localStorage.setItem('tabs', 0);
+
+    } else {
+
+        var index = localStorage.getItem('tabs');
+
+        console.log(index);
+        var $tab = $tabs.eq(index);
+        $tab.addClass('show active');
+        $($tab.attr('href')).addClass('show active');
+    }
+}
+
